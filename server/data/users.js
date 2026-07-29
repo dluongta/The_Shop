e139421fdd0419a862394586a808cbf9f1915cee
Luong <ctpluong@gmail.com>
2026-07-29 11:23:40 +0700

    complete app

--- server/data/users.js
+++ server/data/users.js
@@ -7,7 +7,6 @@ const users = [
     password: '123456',
     role: 'seller',
     isAdmin: true,
-    // Add default discount codes
     discounts: ['DISCOUNT10', 'DISCOUNT20', 'SALE10'],
     paypalClientId: 'AfW47Nj0c4k_bHHB5Kn1a0EYKCoe5nBDxG_fcraZiuEoSyxC9IRvBn7kIj6Qkcy7o3lU18TVYZzt9Nid'
   },
@@ -16,21 +15,18 @@ const users = [
     email: 'jane@example.com',
     role: 'buyer',
     password: '123456',
-    // Add default discount codes
     discounts: ['DISCOUNT10', 'DISCOUNT20', 'SALE10'],
     paypalClientId: 'AfW47Nj0c4k_bHHB5Kn1a0EYKCoe5nBDxG_fcraZiuEoSyxC9IRvBn7kIj6Qkcy7o3lU18TVYZzt9Nid'
   },
 ];
 
-// Optional: Seed more users if needed
 /*
 for (let i = 1; i <= 100; i++) {
   const newUser = {
     name: `User ${i}`,
     email: `user${i}@example.com`,
     password: bcrypt.hashSync('123456', 10),
-    discounts: ['DISCOUNT10', 'DISCOUNT20', 'SALE10'],  // Add default discounts
-  };
+    discounts: ['DISCOUNT10', 'DISCOUNT20', 'SALE10'],
   users.push(newUser);
 }
 */
