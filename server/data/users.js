cdf4b7598510f6809aa28fd8d049376569b168d6
Luong <ctpluong@gmail.com>
2026-09-07 20:58:41 +0700

    complete app

--- server/data/users.js
+++ server/data/users.js
@@ -19,6 +19,16 @@ const users = [
     discounts: ['DISCOUNT10', 'DISCOUNT20', 'SALE100'],
     paypalClientId: 'AfW47Nj0c4k_bHHB5Kn1a0EYKCoe5nBDxG_fcraZiuEoSyxC9IRvBn7kIj6Qkcy7o3lU18TVYZzt9Nid'
   },
+  {
+    name: 'Lucien Street',
+    email: 'lucien@example.com',
+    role: 'seller',
+    isAdmin: false,
+    isVerified: true,
+    password: '123456',
+    discounts: ['DISCOUNT10', 'DISCOUNT20', 'SALE100'],
+    paypalClientId: 'AfW47Nj0c4k_bHHB5Kn1a0EYKCoe5nBDxG_fcraZiuEoSyxC9IRvBn7kIj6Qkcy7o3lU18TVYZzt9Nid'
+  },
 ];
 
 /*
@@ -29,7 +39,7 @@ for (let i = 1; i <= 100; i++) {
     role: 'buyer',
     isAdmin: false,
     isVerified: true,
-    password: ='123456',
+    password: '123456',
     discounts: ['DISCOUNT10', 'DISCOUNT20', 'SALE100'],
   users.push(newUser);
 }
